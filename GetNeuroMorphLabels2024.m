2bfbe90b898c4397612b08008bd83d66eb073159
Leyla Loued-Khenissi <38521532+LLouedKhen@users.noreply.github.com>
2024-04-30 10:35:26 +0200

    Add files via upload

---
+++ GetNeuroMorphLabels2024.m
@@ -0,0 +1,22 @@
+cd '/Users/sysadmin/Documents/UniGe_ToPLaB/Recovered/ImagingData/SecondLevel_Analyses/Model42/Pain_O'
+load SPM.mat
+
+cd '/Users/sysadmin/Documents/UniGe_ToPLaB/Recovered/ImagingData/MediationAnalyses'
+files = dir('X-M-Y_pvals_*.csv');
+for i = 1:length(files)
+thisFile = files(i).name;
+t = readtable(thisFile);
+theseC = table2array(t(:, 2:4));
+XYZmm = theseC';
+xA = spm_atlas('load','neuromorphometrics');
+%XYZmm = [TabDat.dat{:,end}];
+label = {};
+for j=1:size(XYZmm,2)
+  label{j} = spm_atlas('query',xA,XYZmm(:,j));
+end
+labels = label';
+t.Region = labels;
+writetable(t,[thisFile(1:end-4), 'Abdridged.csv']);
+file = [thisFile(1:end-4), 'labels.mat'];
+save(file, 'labels');
+end
